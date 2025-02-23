107c5f9d8affe8f46beb0c88ffaf0fee7baf3c30
Rabeya <rabeyaislam.zisan@gmail.com>
2025-02-23 16:29:25 +0600

    restarting

--- src/test/java/chromeClass.java
+++ src/test/java/chromeClass.java
@@ -2,7 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class firstClass {
+public class chromeClass {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
--- src/test/java/firefoxClass.java
+++ src/test/java/firefoxClass.java
@@ -2,12 +2,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class thirdClass {
+public class firefoxClass {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.firefoxdriver().setup();
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.get("https://softekogradecalculator.netlify.app/calculator/grade-calculator");
+        driver.get("https://google.com");
 
         Thread.sleep(2000);
         driver.quit();
